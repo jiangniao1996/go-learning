f4f15137a244e2c27a9f0eaa09b41cbc58ce2391
Niao <jiangniao1996@qq.com>
2022-03-08 00:50:30 +0800

    X

--- src/ch6/array_test/array_test.go
+++ src/ch6/array_test/array_test.go
@@ -5,10 +5,11 @@ import "testing"
 func TestArrayInit(t *testing.T) {
 	var arr [3]int
 	arr1 := [4]int{1, 2, 3, 4}
-	arr3 := [...]int{1, 2, 3, 4, 5}
-	arr1[1] = 5
-	t.Log(arr[1], arr[2])
-	t.Log(arr1, arr3)
+	arr3 := [...]int{1, 2, 5}
+	arr[1] = 5
+	t.Log(arr[1])
+	t.Log(arr1[3])
+	t.Log(arr3)
 }
 
 func TestArrayTravel(t *testing.T) {
@@ -19,10 +20,16 @@ func TestArrayTravel(t *testing.T) {
 	for idx, e := range arr3 {
 		t.Log(idx, e)
 	}
+
+	for _, e := range arr3 {
+		t.Log(e)
+	}
 }
 
 func TestArraySection(t *testing.T) {
 	arr3 := [...]int{1, 2, 3, 4, 5}
 	arr3_sec := arr3[:3]
+	arr4_sec := arr3[:len(arr3)]
 	t.Log(arr3_sec)
+	t.Log(arr4_sec)
 }
