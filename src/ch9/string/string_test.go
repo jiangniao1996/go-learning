1126dc3318e8a5ef94a6a68ee83bde23169866d1
jiangniao1996 <1027171964@qq.com>
2022-03-12 18:31:52 +0800

    x

--- src/ch9/string/string_test.go
+++ src/ch9/string/string_test.go
@@ -1,7 +1,17 @@
 package my_string
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestStringToRune(t *testing.T) {
+	s := "客"
+	t.Log(len(s))
+	c := []rune(s)
+	t.Log(len(c))
 
+	s = "中华人民共和国"
+	for _, d := range s {
+		t.Logf("%[1]c %[1]x %[1]d", d)
+	}
 }
